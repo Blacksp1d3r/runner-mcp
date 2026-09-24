ec79ac228d96aebf8b3413367dd87b7353051138
Blacksp1d3r <44021891+Blacksp1d3r@users.noreply.github.com>
2026-09-24 13:51:03 +0200

    refactor: rebase adapter recipes after pruning review

--- src/runner_mcp/adapters/__init__.py
+++ src/runner_mcp/adapters/__init__.py
@@ -1,9 +1,22 @@
-from .registry import AdapterError, adapter_ids, get_adapter, inspect_project, list_adapters
+from .base import MigrationPresetRecipe, TestPresetRecipe
+from .registry import (
+    AdapterError,
+    adapter_ids,
+    get_adapter,
+    inspect_project,
+    list_adapters,
+    materialize_migration_preset,
+    materialize_test_preset,
+)
 
 __all__ = [
     "AdapterError",
+    "MigrationPresetRecipe",
+    "TestPresetRecipe",
     "adapter_ids",
     "get_adapter",
     "inspect_project",
     "list_adapters",
+    "materialize_migration_preset",
+    "materialize_test_preset",
 ]
